d86f861a996bed80a092f9e8776f4d5cf1f5c503
Austen Cabret <austencabret@gmail.com>
2024-03-01 02:20:16 -0500

    testing typescript error fix in index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,17 @@ import "./App.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement !== null) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Element with id 'root' not found");
+}
